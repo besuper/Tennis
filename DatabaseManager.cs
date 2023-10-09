14e536ab57b27eda2e3fe98dfde05d863b9190df
besuper <16976439+besuper@users.noreply.github.com>
2023-10-09 21:45:13 +0200

    first commit

---
+++ DatabaseManager.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace database
+{
+    internal class DatabaseManager
+    {
+        private static SqlConnection _connection = new SqlConnection(@"Server=mauvechemineeposeur.ddns.net;Database=bd1;User ID=sa;Password=<Minecraft123>;Encrypt=no");
+
+        public static SqlConnection GetConnection()
+        {
+            if (_connection == null)
+            {
+                _connection = new SqlConnection();
+            }
+
+            if (_connection.State == ConnectionState.Open)
+            {
+                return _connection;
+            }
+
+            _connection.Open();
+
+            return _connection;
+        }
+
+        public SqlDataReader Get(string sql)
+        {
+            SqlCommand command = new SqlCommand(sql, GetConnection());
+            SqlDataReader reader = command.ExecuteReader();
+
+            return reader;
+        }
+
+        public SqlDataReader PreparedGet(string sql, params SqlParameter[] sqlParameter)
+        {
+            SqlCommand cmd = new SqlCommand(sql, GetConnection());
+
+            foreach (SqlParameter param in sqlParameter)
+            {
+                cmd.Parameters.Add(param);
+            }
+
+            cmd.CommandType = CommandType.Text;
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            return reader;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using database;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Reflection.PortableExecutable;
+using System;
+
+namespace database
+{
+    internal class Program
+    {
+        public static DatabaseManager databaseManager = new DatabaseManager();
+
+        static void Main(string[] args)
+        {
+            PreparedGet();
+        }
+
+        static void Get()
+        {
+            // Simple get example
+            SqlDataReader request = databaseManager.Get("SELECT * FROM Joueur");
+
+            while (request.Read())
+            {
+                Console.WriteLine("User id: " + request.GetValue(0));
+                Console.WriteLine("Nom: " + request.GetValue(1));
+                Console.WriteLine("Prenom: " + request.GetValue(2));
+            }
+        }
+
+        static void PreparedGet()
+        {
+            // Prepared GET example
+            SqlParameter param1 = new SqlParameter("@param1", SqlDbType.Int);
+            param1.Value = 15;
+
+            SqlDataReader request2 = databaseManager.PreparedGet("SELECT * FROM Joueur WHERE id_joueur = @param1", param1);
+
+            if (request2.Read())
+            {
+                Console.WriteLine("User id: " + request2.GetValue(0));
+                Console.WriteLine("Nom: " + request2.GetValue(1));
+                Console.WriteLine("Prenom: " + request2.GetValue(2));
+            }
+        }
+    }
+}
